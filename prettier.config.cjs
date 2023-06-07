2873572a08348d40ae12cc12ecc051ffe97d69af
Ikraam <ibndaanis@gmail.com>
2023-06-07 22:00:51 +0100

    prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,21 @@
 /** @type {import("prettier").Config} */
 const config = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false,
 };
 
 module.exports = config;
