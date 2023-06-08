86200428c4d4aeede7743c2e7a483bfd69be4f12
Ikraam <ibndaanis@gmail.com>
2023-06-08 19:47:49 +0100

    added filtering to the table. filter by month.

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,14 +1,16 @@
 import {
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
   type ColumnDef,
+  type ColumnFiltersState,
+  type FilterFn,
   type SortingState
 } from "@tanstack/react-table";
 import { useState } from "react";
-
 import {
   Table,
   TableBody,
@@ -17,11 +19,15 @@ import {
   TableHeader,
   TableRow
 } from "~/components/ui/table";
+import { Input } from "./ui/input";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
   isLoading?: boolean;
+  filter?: keyof TData;
+  filterPlaceholder?: string;
+  filterFunctions?: Record<string, FilterFn<TData>> | undefined;
 }
 
 /**
@@ -30,9 +36,13 @@ interface DataTableProps<TData, TValue> {
 export function DataTable<TData, TValue>({
   columns,
   data,
-  isLoading
+  isLoading,
+  filter,
+  filterPlaceholder,
+  filterFunctions
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const loadingSkeletons = Array.from(Array(12));
 
@@ -42,68 +52,94 @@ export function DataTable<TData, TValue>({
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    filterFns: filterFunctions || {},
     state: {
-      sorting
+      sorting,
+      columnFilters
     }
   });
 
   return (
-    <div className="w-full rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map(headerGroup => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody className="min-h-[600px]">
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map(row => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map(cell => {
+    <div className="flex w-full flex-col">
+      {filter && (
+        <div className="flex items-center py-4">
+          <Input
+            placeholder={filterPlaceholder || "Filter..."}
+            value={
+              (table.getColumn(filter as string)?.getFilterValue() as string) ??
+              ""
+            }
+            onChange={event =>
+              table
+                .getColumn(filter as string)
+                ?.setFilterValue(event.target.value.trim())
+            }
+            className="max-w-sm"
+          />
+        </div>
+      )}
+      <div className="w-full rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
                   return (
-                    <TableCell key={cell.id}>
-                      {isLoading ? (
-                        <div
-                          key={cell.id}
-                          className={`h-4 animate-pulse rounded bg-slate-800`}
-                        ></div>
-                      ) : (
-                        flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )
-                      )}
-                    </TableCell>
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
                   );
                 })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody className="min-h-[600px]">
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map(cell => {
+                    return (
+                      <TableCell key={cell.id}>
+                        {isLoading ? (
+                          <div
+                            key={cell.id}
+                            className={`h-4 animate-pulse rounded bg-slate-800`}
+                          ></div>
+                        ) : (
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -1,10 +1,17 @@
 import { type OperatorTransaction } from "@prisma/client";
-import { type ColumnDef } from "@tanstack/react-table";
+import type { ColumnDef, FilterFn, Row } from "@tanstack/react-table";
 import dayjs from "dayjs";
+import { ArrowUpDown } from "lucide-react";
 import { api } from "~/utils/api";
 import { DataTable } from "./DataTable";
 import { Button } from "./ui/button";
-import { ArrowUpDown } from "lucide-react";
+
+// Making a custom filter function for dates.
+declare module "@tanstack/table-core" {
+  interface FilterFns {
+    date: FilterFn<unknown>;
+  }
+}
 
 /**
  * Columns for the transactions table.
@@ -25,48 +32,79 @@ const columns: ColumnDef<OperatorTransaction>[] = [
     },
     cell: data => {
       const timeUnit = data.getValue<Date>();
-      return (
-        <span className="pl-4">{dayjs(timeUnit).format("MMMM YYYY")}</span>
-      );
-    }
+      return <p className="pl-4">{dayjs(timeUnit).format("MMMM YYYY")}</p>;
+    },
+    filterFn: "date"
   },
   {
     accessorKey: "usdEarned",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          USD Earned
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex items-center justify-center">
+          <Button
+            variant="ghost"
+            className="w-fit min-w-[160px]"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            USD Earned ($)
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
       );
     },
     cell: data => {
-      const gasCosts = data.getValue<Date>();
-      return <span className="pl-4">{gasCosts.toLocaleString("en-US")}</span>;
+      const usdEarned = data.getValue<number>();
+      return (
+        <p className="text-center">
+          {Number(usdEarned.toFixed(2)).toLocaleString("en-us")}
+        </p>
+      );
     }
   },
   {
     accessorKey: "gasCosts",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Gas Costs
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex items-center justify-center">
+          <Button
+            variant="ghost"
+            className="w-fit min-w-[160px]"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Gas Costs ($)
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
       );
     },
     cell: data => {
-      const gasCosts = data.getValue<Date>();
-      return <span className="pl-4">{gasCosts.toLocaleString("en-US")}</span>;
+      const gasCosts = data.getValue<number>();
+      return (
+        <p className="text-center">
+          {Number(gasCosts.toFixed(2)).toLocaleString("en-us")}
+        </p>
+      );
     }
   }
 ];
+
+/**
+ * Since we are formatting the dates to `MONTH YEAR`,
+ * we should make a custom filter so users can type
+ * in the month and/or year number.
+ */
+function monthFilter(
+  row: Row<OperatorTransaction>,
+  key: string,
+  filterQuery: string
+) {
+  const timeUnit = row.getValue<Date>(key);
+
+  return dayjs(timeUnit)
+    .format("MMMM YYYY")
+    .toLowerCase()
+    .includes(filterQuery.trim().toLowerCase());
+}
 
 /**
  * Table displaying the transactions.
@@ -76,13 +114,20 @@ export const TransactionsTable = () => {
     api.transactionsRouter.getAll.useQuery();
 
   return (
-    <div className="container flex h-full flex-col items-center justify-center">
-      <h2 className="mb-12 text-5xl font-bold">Monthly Transactions</h2>
-      <div className="flex w-full max-w-[800px] cursor-pointer items-center justify-center overflow-hidden rounded-md bg-[#0a0a0a]">
+    <div className="container flex h-full flex-col items-center pt-[10rem]">
+      <h2 className="mb-12 text-center text-3xl font-bold sm:text-4xl md:text-5xl">
+        Monthly Transactions
+      </h2>
+      <div className="flex w-full max-w-[800px] cursor-pointer items-center justify-center rounded-md bg-[#0a0a0a]">
         <DataTable
           columns={columns}
           data={transactions || []}
           isLoading={isLoading}
+          filter="timeUnit"
+          filterPlaceholder="Filter by month..."
+          filterFunctions={{
+            date: monthFilter
+          }}
         />
       </div>
     </div>
